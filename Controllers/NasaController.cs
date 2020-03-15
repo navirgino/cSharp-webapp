c7e20982c70669b5dd5266d005b1acdfd87899ec
Nico Virgino <nicovirgino@gmail.com>
2020-03-14 23:39:11 -0500

    working on hooking up nasa api call to .net framework

---
+++ Controllers/NasaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using cSharp_webapp.Models
+using System.Web.Http;
+
+namespace cSharp_webapp.Controllers
+{
+    public class NasaController : ApiController
+    {
+        public string GetNasaPics(string JSONString)
+        {
+            var seriptSerialization = new System.Web.Script
+                .Serialization.JavaScriptSerializer();
+            NasaPicture np = seriptSerialization
+                 .Deserialize<NasaPicture>(JSONString);
+            return "title - " + np.npDetails.title +
+                "description - " + np.npDetails.description;
+        }
+
+
+    }
+}
---
+++ Models/NasaPicture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cSharp_webapp.Models
+{
+    public class NasaPicture
+    {
+      
+        public NasaPictureDetails npDetails { get; set; }
+      
+    }
+    public class NasaPictureDetails
+    {
+        public string title { get; set; }
+        public string location { get; set; }
+        public string date_created { get; set; }
+        public string description { get; set; }
+
+    }
+}
