8ba2eb7a5ceb908bb530a8cbcecba7f298b78474
Nico Virgino <nicovirgino@gmail.com>
2020-03-14 22:53:59 -0500

    added some js logic for later implementation for the api

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+var nasaPicsUri = '/api/pics'
+
+function ajaxHelper(uri, method, data) {
+    self.error('') //clear error msg
+    return $.ajax({
+        type: method,
+        url: uri,
+        dataType: 'json',
+        contentType: 'application/json',
+        data: data ? JSON.stringify(data) : null
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        self.error(errorThrown);
+    });
+
+}
