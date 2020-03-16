badc0247d7842aee9c7e08b809b50830946eef12
Nico Virgino <nicovirgino@gmail.com>
2020-03-16 14:10:25 -0500

    working on parallax for view

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace cSharp_webapp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/js/site").Include(
+                "~/Scripts/app.js"));
           
         }
     }
--- Scripts/app.js
+++ Scripts/app.js
@@ -1,17 +1,27 @@
 ﻿"use strict";
 
-var nasaPicsUri = '/api/pics'
+//var nasaPicsUri = '/api/pics'
 
-function ajaxHelper(uri, method, data) {
-    self.error('') //clear error msg
-    return $.ajax({
-        type: method,
-        url: uri,
-        dataType: 'json',
-        contentType: 'application/json',
-        data: data ? JSON.stringify(data) : null
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-        self.error(errorThrown);
-    });
+//function ajaxHelper(uri, method, data) {
+//    self.error('') //clear error msg
+//    return $.ajax({
+//        type: method,
+//        url: uri,
+//        dataType: 'json',
+//        contentType: 'application/json',
+//        data: data ? JSON.stringify(data) : null
+//    }).fail(function (jqXHR, textStatus, errorThrown) {
+//        self.error(errorThrown);
+//    });
 
-}
+//}
+
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
+
