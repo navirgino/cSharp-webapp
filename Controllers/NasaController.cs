f811c4aa4ea084d9f0190ebac56c44bd4456cde4
Nico Virgino <nicovirgino@gmail.com>
2020-03-15 23:40:15 -0500

    cleaning up code for build efficiency

--- Controllers/NasaController.cs
+++ Controllers/NasaController.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using cSharp_webapp.Models
-using System.Web.Http;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using cSharp_webapp.Models
+//using System.Web.Http;
 
-namespace cSharp_webapp.Controllers
-{
-    public class NasaController : ApiController
-    {
-        public string GetNasaPics(string JSONString)
-        {
-            var seriptSerialization = new System.Web.Script
-                .Serialization.JavaScriptSerializer();
-            NasaPicture np = seriptSerialization
-                 .Deserialize<NasaPicture>(JSONString);
-            return "title - " + np.npDetails.title +
-                "description - " + np.npDetails.description;
-        }
+//namespace cSharp_webapp.Controllers
+//{
+//    public class NasaController : ApiController
+//    {
+//        public string GetNasaPics(string JSONString)
+//        {
+//            var seriptSerialization = new System.Web.Script
+//                .Serialization.JavaScriptSerializer();
+//            NasaPicture np = seriptSerialization
+//                 .Deserialize<NasaPicture>(JSONString);
+//            return "title - " + np.npDetails.title +
+//                "description - " + np.npDetails.description;
+//        }
 
 
-    }
-}
+//    }
+//}
